d3320648e78f045eed73ccf3d48b2e6c44e9ab3d
jensr805-lang <jensr805@gmail.com>
2026-05-26 15:40:42 +0200

    Dev_Jens 1

--- testie/Program.cs
+++ testie/Program.cs
@@ -1,8 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var kat = new Kat("Minoes", 3);
-Console.WriteLine($"Naam: {kat.Naam}, Leeftijd: {kat.Leeftijd}");
-kat.Miauw();
+var katten = new List<Kat>
+{
+    new Kat("Minoes", 3),
+    new Kat("Snorremans", 5),
+    new Kat("Poesje", 1)
+};
+
+Console.WriteLine("Overzicht van katten:");
+for (int i = 0; i < katten.Count; i++)
+{
+    var kat = katten[i];
+    Console.WriteLine($"{i + 1}. Naam: {kat.Naam}, Leeftijd: {kat.Leeftijd}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Alle katten laten miauwen:");
+foreach (var kat in katten)
+{
+    kat.Miauw();
+}
 
 public class Kat
 {
@@ -17,6 +34,6 @@ public class Kat
 
     public void Miauw()
     {
-        Console.WriteLine("Miauw!");
+        Console.WriteLine($"{Naam} zegt: Miauw!");
     }
 }
