e301f8d72c44a152ac871766f929df3fb07fef8b
Thelegendofboydens <arnaudB803@lln.campussintursula.be>
2026-05-26 15:34:07 +0200

    test1

---
+++ testie/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var kat = new Kat("Minoes", 3);
+Console.WriteLine($"Naam: {kat.Naam}, Leeftijd: {kat.Leeftijd}");
+kat.Miauw();
+
+public class Kat
+{
+    public string Naam { get; set; }
+    public int Leeftijd { get; set; }
+
+    public Kat(string naam, int leeftijd)
+    {
+        Naam = naam;
+        Leeftijd = leeftijd;
+    }
+
+    public void Miauw()
+    {
+        Console.WriteLine("Miauw!");
+    }
+}
